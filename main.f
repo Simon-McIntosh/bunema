974ee568ab6ca0a849c7ff7ea9e913a5d95b6176
Simon McIntosh <simon.mcintosh@iter.org>
2025-03-21 15:20:52 +0100

    minimal working example with static input and referance output

--- main.f
+++ main.f
@@ -2,23 +2,35 @@
       implicit none
       
       ! Declare variables
-      real*8 :: psi(100), sia(100)
-      integer :: nwb, nhb, nwnh, i
+      integer, parameter :: nwnh = 8385
+      real*8 :: psi(nwnh), sia(nwnh)
+      real*8 :: s,shift,dr,dz
+      integer :: m,n
+      common/bunemn/m,n,s,shift,dr,dz
+      integer :: nwb, nhb, i
+      integer :: nbunema = 55
 
       ! Initialize variables
-      nwb = 10
-      nhb = 10
-      nwnh = 100
-
-      ! Fill psi and sia with random values to avoid zero division
-      call random_seed()
-      do i = 1, 100
-        call random_number(psi(i))
-        call random_number(sia(i))
-      end do
+      open (unit=nbunema,file='bunema_in.dat',form='formatted')    
+      read (nbunema,2022) m,n,nwb,nhb
+      read (nbunema,2020) s,shift,dr,dz
+      read (nbunema,2020) (psi(i),i=1,nwnh)
+      close(nbunema)
 
       ! Call the subroutine
       call buneto(psi, nwb, nhb, sia, nwnh)
+
+      ! Write the output to a file
+      open (unit=nbunema,file='bunema_out.dat',form='formatted')
+      write (nbunema,2022) m,n,nwb,nhb
+      write (nbunema,2020) s,shift,dr,dz
+      write (nbunema,2020) (psi(i),i=1,nwnh)
+      write (nbunema,2020) (sia(i),i=1,nwnh)
+      close(nbunema)
+
+
+2020  format (4e16.9)
+2022  format (5i5)
 
       ! End the program
       stop
