f80e7fd7af320d51e56a3a1fbfc95635789cdbe4
Max Dubrov <comrademax@yandex.ru>
2025-03-24 17:02:37 +0100

    Correct input data read format

--- main.f
+++ main.f
@@ -2,34 +2,41 @@
       implicit none
       
       ! Declare variables
-      integer, parameter :: nwnh = 8385
-      real*8 :: psi(nwnh), sia(nwnh)
+      integer, parameter :: nwnh0 = 8385
+      real*8 :: psi(nwnh0), sia(nwnh0)
       real*8 :: s,shift,dr,dz
       integer :: m,n
       common/bunemn/m,n,s,shift,dr,dz
-      integer :: nwb, nhb, i
-      integer :: nbunema = 55
+      integer :: nwb, nhb, nwnh
+      integer :: i, nbunema = 55
 
       ! Initialize variables
       open (unit=nbunema,file='bunema_in.dat',form='formatted')    
-      read (nbunema,2022) m,n,nwb,nhb
+      read (nbunema,2022) m,n,nwb,nhb,nwnh
+      write(*,*) 'nwnh =', nwnh
       read (nbunema,2020) s,shift,dr,dz
       read (nbunema,2020) (psi(i),i=1,nwnh)
+      read (nbunema,2022) nwnh
+      write(*,*) 'nwnh =', nwnh
+      read (nbunema,2020) (sia(i),i=1,nwnh)
+      read (nbunema,2022) nwnh
+      write(*,*) 'nwnh =', nwnh
       close(nbunema)
+
+
+
+      ! Fill psi and sia with random values to avoid zero division
+      !call random_seed()
+      !do i = 1, 100
+      !  call random_number(psi(i))
+      !  call random_number(sia(i))
+      !end do
 
       ! Call the subroutine
       call buneto(psi, nwb, nhb, sia, nwnh)
 
-      ! Write the output to a file
-      open (unit=nbunema,file='bunema_out.dat',form='formatted')
-      write (nbunema,2022) m,n,nwb,nhb
-      write (nbunema,2020) s,shift,dr,dz
-      write (nbunema,2020) (psi(i),i=1,nwnh)
-      write (nbunema,2020) (sia(i),i=1,nwnh)
-      close(nbunema)
 
-
-2020  format (4e16.9)
+2020  format (5e16.9)
 2022  format (5i5)
 
       ! End the program
